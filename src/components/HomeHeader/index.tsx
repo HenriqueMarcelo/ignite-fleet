b6930e469c368626e3bd21382fbccb4babced961
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-08-03 13:16:18 -0300

    feat: get user info

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -3,17 +3,20 @@ import { Container, Greeting, Message, Name, Picture } from './styles'
 import { TouchableOpacity } from 'react-native'
 import { Power } from 'phosphor-react-native'
 import theme from '../../theme'
+import { useUser } from '@realm/react'
 
 export function HomeHeader() {
+  const user = useUser()
+
   return (
     <Container>
       <Picture
-        source={{ uri: 'https://github.com/henriquemarcelo.png' }}
+        source={{ uri: user.profile.pictureUrl }}
         placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
       />
       <Greeting>
         <Message>Olá</Message>
-        <Name>Marcelo</Name>
+        <Name>{user.profile.name}</Name>
       </Greeting>
       <TouchableOpacity>
         <Power size={32} color={theme.COLORS.GRAY_400} />
