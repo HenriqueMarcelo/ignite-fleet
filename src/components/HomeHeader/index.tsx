a0683ecdfa8b295f759e366b4a7564e873eef0d2
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-08-03 14:18:45 -0300

    feat: use safe area

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -4,17 +4,21 @@ import { TouchableOpacity } from 'react-native'
 import { Power } from 'phosphor-react-native'
 import theme from '../../theme'
 import { useApp, useUser } from '@realm/react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export function HomeHeader() {
   const user = useUser()
   const app = useApp()
+
+  const insets = useSafeAreaInsets()
+  const paddingTop = insets.top + 32
 
   function handleLogOut() {
     app.currentUser?.logOut()
   }
 
   return (
-    <Container>
+    <Container style={{ paddingTop }}>
       <Picture
         source={{ uri: user.profile.pictureUrl }}
         placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
