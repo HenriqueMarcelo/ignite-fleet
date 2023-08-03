5d08ed3a5a7ce7f194092e827d83cb41f5422c41
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-08-03 13:14:31 -0300

    feat: show user picture

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Greeting, Message, Name } from './styles'
+import { Container, Greeting, Message, Name, Picture } from './styles'
 import { TouchableOpacity } from 'react-native'
 import { Power } from 'phosphor-react-native'
 import theme from '../../theme'
@@ -7,6 +7,10 @@ import theme from '../../theme'
 export function HomeHeader() {
   return (
     <Container>
+      <Picture
+        source={{ uri: 'https://github.com/henriquemarcelo.png' }}
+        placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
+      />
       <Greeting>
         <Message>Olá</Message>
         <Name>Marcelo</Name>
