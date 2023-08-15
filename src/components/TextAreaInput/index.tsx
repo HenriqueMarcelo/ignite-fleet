009a20939f3af6e6c1af2d174217f04b2adc9639
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-08-15 11:26:24 -0300

    feat: use ref on the inputs

--- src/components/TextAreaInput/index.tsx
+++ src/components/TextAreaInput/index.tsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import { forwardRef } from 'react'
 import { Container, Input, Label } from './styles'
 import { useTheme } from 'styled-components/native'
-import { TextInputProps } from 'react-native'
+import { TextInput, TextInputProps } from 'react-native'
 
 type Props = TextInputProps & {
   label: string
 }
 
-export function TextAreaInput({ label, ...rest }: Props) {
-  const theme = useTheme()
+const TextAreaInput = forwardRef<TextInput, Props>(
+  ({ label, ...rest }, ref) => {
+    const theme = useTheme()
 
-  return (
-    <Container>
-      <Label>{label}</Label>
-      <Input
-        autoCapitalize="sentences"
-        placeholderTextColor={theme.COLORS.GRAY_400}
-        multiline
-        {...rest}
-      />
-    </Container>
-  )
-}
+    return (
+      <Container>
+        <Label>{label}</Label>
+        <Input
+          ref={ref}
+          autoCapitalize="sentences"
+          placeholderTextColor={theme.COLORS.GRAY_400}
+          multiline
+          {...rest}
+        />
+      </Container>
+    )
+  },
+)
+
+TextAreaInput.displayName = 'TextAreaInput'
+
+export { TextAreaInput }
