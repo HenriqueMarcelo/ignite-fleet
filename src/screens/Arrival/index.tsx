773ff76668543855999b66cd00316bdfa983553f
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-08-18 13:38:00 -0300

    feat: create Arrival screen

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,5 +1,14 @@
 import { useRoute } from '@react-navigation/native'
-import { Container } from './styles'
+import {
+  Container,
+  Content,
+  Description,
+  Footer,
+  Label,
+  LicensePlate,
+} from './styles'
+import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
 
 type RouteParamsProps = {
   id: string
@@ -8,6 +17,26 @@ type RouteParamsProps = {
 export function Arrival() {
   const route = useRoute()
   const { id } = route.params as RouteParamsProps
-  console.log('Arrival ID: ', id)
-  return <Container></Container>
+
+  return (
+    <Container>
+      <Header title="Chegada" />
+      <Content>
+        <Label>Placa do veículo</Label>
+        <LicensePlate>XXX0000</LicensePlate>
+
+        <Label>Finalidade</Label>
+        <Description>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veniam
+          assumenda dicta, odit inventore, id, blanditiis iste repudiandae et
+          est totam eaque aliquid officiis nostrum! Ab, illum tempore? Neque,
+          blanditiis.
+        </Description>
+
+        <Footer>
+          <Button title="Registrar chegada" />
+        </Footer>
+      </Content>
+    </Container>
+  )
 }
