2cf4f495066d0d080b914a59bf000f5736fe5ef5
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-08-18 13:47:04 -0300

    feat: create ButtonIcon component

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,19 @@
+import { TouchableOpacityProps } from 'react-native'
+import { Container } from './styles'
+import { IconProps } from 'phosphor-react-native'
+import { useTheme } from 'styled-components/native'
+
+export type IconBoxProps = (props: IconProps) => JSX.Element
+
+type Props = TouchableOpacityProps & {
+  icon: IconBoxProps
+}
+
+export function ButtonIcon({ icon: Icon, ...rest }: Props) {
+  const theme = useTheme()
+  return (
+    <Container activeOpacity={0.7} {...rest}>
+      <Icon size={24} color={theme.COLORS.BRAND_MID} />
+    </Container>
+  )
+}
--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -9,6 +9,8 @@ import {
 } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { ButtonIcon } from '../../components/ButtonIcon'
+import { X } from 'phosphor-react-native'
 
 type RouteParamsProps = {
   id: string
@@ -34,6 +36,7 @@ export function Arrival() {
         </Description>
 
         <Footer>
+          <ButtonIcon icon={X} />
           <Button title="Registrar chegada" />
         </Footer>
       </Content>
