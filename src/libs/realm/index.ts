182ea184fa51ae8cacb1bda7372f9c8bed30544c
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-08-21 11:06:19 -0300

    feat: sync db

--- src/libs/realm/index.ts
+++ src/libs/realm/index.ts
@@ -1,6 +1,17 @@
 import { createRealmContext } from '@realm/react'
 import { Historic } from './schemas/Historic'
 
+const realmFileBehavior: Realm.OpenRealmBehaviorConfiguration = {
+  type: Realm.OpenRealmBehaviorType.OpenImmediately,
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const syncConfig: any = {
+  flexible: true,
+  newRealmFileBehavior: realmFileBehavior,
+  existingRealmFileBehavior: realmFileBehavior,
+}
+
 export const { RealmProvider, useRealm, useQuery, useObject } =
   createRealmContext({
     schema: [Historic],
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from 'react'
 import { Alert, FlatList } from 'react-native'
 import { HistoricCard, HistoricCardProps } from '../../components/HistoricCard'
 import dayjs from 'dayjs'
+import { useUser } from '@realm/react'
 
 export function Home() {
   const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null)
@@ -17,6 +18,7 @@ export function Home() {
   const { navigate } = useNavigation()
   const historic = useQuery(Historic)
   const realm = useRealm()
+  const user = useUser()
 
   function handleRegisterMovement() {
     if (vehicleInUse?._id) {
@@ -90,6 +92,17 @@ export function Home() {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  useEffect(() => {
+    realm.subscriptions.update((mutableSubs, realm) => {
+      const historicByUserQuery = realm
+        .objects('Historic')
+        .filtered(`user_id = '${user.id}'`)
+
+      mutableSubs.add(historicByUserQuery, { name: 'historic_by_user' })
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [realm])
 
   return (
     <Container>
