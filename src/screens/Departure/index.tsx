e775836e9dc4f19565d3de14a7a80f88bf0c96cb
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-08-22 11:20:45 -0300

    feat: get position

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -11,7 +11,12 @@ import { Historic } from '../../libs/realm/schemas/Historic'
 import { useUser } from '@realm/react'
 import { useNavigation } from '@react-navigation/native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { useForegroundPermissions } from 'expo-location'
+import {
+  useForegroundPermissions,
+  watchPositionAsync,
+  LocationAccuracy,
+  LocationSubscription,
+} from 'expo-location'
 
 export function Departure() {
   const [description, setDescription] = useState('')
@@ -69,6 +74,26 @@ export function Departure() {
   useEffect(() => {
     requestLocationForegroundPermission()
   }, [])
+
+  useEffect(() => {
+    if (!locationForegroundPermission?.granted) {
+      return
+    }
+
+    let subscription: LocationSubscription
+
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.High,
+        timeInterval: 1000,
+      },
+      (location) => {
+        console.log(location)
+      },
+    ).then((response) => (subscription = response))
+
+    return () => subscription.remove()
+  }, [locationForegroundPermission?.granted])
 
   if (!locationForegroundPermission?.granted) {
     return (
